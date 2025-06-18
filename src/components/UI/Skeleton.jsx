8ebfb716f27c8a21d1b086c02a00e82edc55933b
Lea <leawork25@gmail.com>
2025-06-17 21:58:57 -0800

    lea-hot-collections

--- src/components/UI/Skeleton.jsx
+++ src/components/UI/Skeleton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Skeleton = ({ width, height, borderRadius }) => {
+const Skeleton = ({ width, height, borderRadius, children }) => {
   return (
     <div
       className="skeleton-box"
@@ -8,8 +8,24 @@ const Skeleton = ({ width, height, borderRadius }) => {
         width,
         height,
         borderRadius,
+        backgroundColor: "#eee",
+        overflow: "hidden",
       }}
-    ></div>
+    >
+      <div className="nft_coll">
+      <div className="nft_wrap">
+        <div className="skeleton skeleton-img" />
+      </div>
+      <div className="nft_coll_pp">
+        <div className="skeleton skeleton-avatar" />
+      </div>
+      <div className="nft_coll_info">
+        <div className="skeleton skeleton-title" />
+        <div className="skeleton skeleton-code" />
+      </div>
+    </div>
+    {children}
+    </div>
   );
 };
 
